e7efd50bb7bee87b699eef04eb9f9c4e9a9b83e6
Spirou1 <enzospironellipinheiro@gmail.com>
2026-09-13 20:06:30 -0300

    integrando criação de administrador

--- src/app/services/auth/LoginService.ts
+++ src/app/services/auth/LoginService.ts
@@ -1,8 +1,17 @@
 import { BaseService } from "../BaseService";
 import { LoginResponse, loginResponseSchema } from "../../models/dto/auth/LoginResponse";
+import {
+  RegisterAdminRequest,
+  RegisterAdminResponse,
+  registerAdminResponseSchema,
+} from "../../models/dto/auth/RegisterAdmin";
 
 export class LoginService extends BaseService {
-    async login(data: unknown): Promise<LoginResponse> {
-        return this.post("/auth/login", data, {}, loginResponseSchema);
-    }
+  async login(data: unknown): Promise<LoginResponse> {
+    return this.post("/auth/login", data, {}, loginResponseSchema);
+  }
+
+  async registerAdmin(data: RegisterAdminRequest): Promise<RegisterAdminResponse> {
+    return this.post("/auth/register-admin", data, {}, registerAdminResponseSchema);
+  }
 }
